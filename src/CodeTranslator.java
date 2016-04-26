31ef10af623f260acb935fce3c0b5fcf978797ae
Ari Zellner <azellner@redhat.com>
2016-04-26 23:22:22 +0300

    Bug fixes and added pointer handling

--- src/CodeTranslator.java
+++ src/CodeTranslator.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * created by Daniel Reisel on 19/04/16.
@@ -8,6 +7,9 @@ public class CodeTranslator {
     String fileName;
     Map<String,String> arithmeticMap;
     Map<String,String> regMap;
+    Map<String,CommandTypes> cmdMap;
+    Set arithmeticSet;
+    final String arithmetics = "add,sub,neg,eq,gt,lt,and,or,not";
     String endLoop = "(ENDLOOP)\n" +
             "@ENDLOOP\n" +
             "0;JMP";
@@ -29,9 +31,20 @@ public class CodeTranslator {
                     "A=M\n" +
                     "M=D\n" ;
 
-
+    public enum CommandTypes{
+        C_ARITHMETIC,
+        C_POP,
+        C_PUSH,
+        C_LABLE,
+        C_GOTO,
+        C_IF,
+        C_FUNCTION,
+        C_RETURN,
+        C_CALL
+    }
 
     public CodeTranslator(String fileName) {
+        this.arithmeticSet = new HashSet(Arrays.asList(arithmetics.split(",")));
         fillArithmeticMap();
         regMap = new HashMap<>();
         regMap.put("local","LCL");
@@ -39,10 +52,31 @@ public class CodeTranslator {
         regMap.put("this","THIS");
         regMap.put("that","THAT");
         regMap.put("temp","R5");
+        regMap.put("static","5");
         this.fileName = fileName;
 
+        cmdMap = new HashMap<>();
+        cmdMap.put("pop",CommandTypes.C_POP);
+        cmdMap.put("push",CommandTypes.C_PUSH);
+        cmdMap.put("goto",CommandTypes.C_GOTO);
+        cmdMap.put("if",CommandTypes.C_IF);
+        cmdMap.put("function",CommandTypes.C_FUNCTION);
+        cmdMap.put("return",CommandTypes.C_RETURN);
+        cmdMap.put("call",CommandTypes.C_CALL);
     }
 
+    CommandTypes commandType(String command){
+        String type = command.split(" ")[0];
+        if(arithmeticSet.contains(type)){
+            return CommandTypes.C_ARITHMETIC;
+        }
+        try {
+            return cmdMap.get(type);
+        } catch (Exception ignored){
+            throw new IllegalArgumentException("Illegal command type");
+        }
+    }
+    
     public String getEndLoop() {
         return endLoop;
     }
@@ -54,18 +88,24 @@ public class CodeTranslator {
     }
 
     String translatePop(String segment, int index){
-
         //handel's this, that, argument, temp and local
         String command = regMap.get(segment);
         if(command != null){
             String setDFormat = "@%s\n" + "D=M\n" + "@%d\n" + "D=D+A\n" + "%s";
             return String.format(setDFormat,command,index,popFormat);
         }
-        if (segment.equals("static")){
-            String setDFormat = "@%s\n" + "%s";
-            return String.format(setDFormat,fileName + "." + index,popFormat);
+        if (segment.equals("pointer")){
+            String setDFormat = "" ;
+            switch (index) {
+                case 1:
+                    setDFormat = "@SP\n" + "M=M-1\n" + "A=M\n" + "D=M\n" + "@THAT\n" + "M=D\n";
+                    break;
+                case 0:
+                    setDFormat = "@SP\n" + "M=M-1\n" + "A=M\n" + "D=M\n" + "@THIS\n" + "M=D\n";
+                    break;
+            }
+            return setDFormat;
         }
-        //TODO : handle pointers.
         throw new IllegalArgumentException("unsupported segment");
     }
 
@@ -74,21 +114,52 @@ public class CodeTranslator {
         //handel's this, that, argument, temp and local
         String command = regMap.get(segment);
         if(command != null){
-            String setDFormat = "@%s\n" + "D=M\n" + "%d\n" + "A=D+A\n" + "D=M\n" + "%s";
+            String setDFormat = "@%s\n" + "D=M\n" + "@%d\n" + "A=D+A\n" + "D=M\n" + "%s";
             return String.format(setDFormat,command,index,pushFormat);
         }
         if (segment.equals("constant")) {
-            String setDFormat = "@%d\n" + "D=A\n" + " %s";
+            String setDFormat = "@%d\n" + "D=A\n" + "%s";
             return String.format(setDFormat, index,pushFormat);
         }
-        if (segment.equals("static")){
-            String setDFormat = "@%s\n" + "%s";
-            return String.format(setDFormat,fileName + "." + index,pushFormat);
+        if (segment.equals("pointer")){
+            String setDFormat = "" ;
+            switch (index) {
+                case 1:
+                    setDFormat = "@THAT\nD=M\n%s";
+                    break;
+                case 0:
+                    setDFormat = "@THIS\nD=M\n%s";
+                    break;
+            }
+            return String.format(setDFormat,pushFormat);
         }
-        //TODO : handle pointers.
         throw new IllegalArgumentException("unsupported segment");
 
     }
+
+    String parseCommand(String command, CommandTypes type){
+        String[] commandArray = command.split(" ");
+        int index;
+        switch (type){
+            case C_ARITHMETIC:
+                return translateArithmetic(commandArray[0]);
+            case C_PUSH:
+                index = Integer.parseInt(commandArray[2]);
+                return translatePush(commandArray[1], index);
+            case C_POP:
+                index = Integer.parseInt(commandArray[2]);
+                return translatePop(commandArray[1],index);
+            case C_LABLE:
+            case C_GOTO:
+            case C_IF:
+            case C_FUNCTION:
+            case C_RETURN:
+            case C_CALL:
+            default:
+                throw new IllegalArgumentException("un supported command type");
+        }
+    }
+
     private void fillArithmeticMap() {
         arithmeticMap = new HashMap<>();
         arithmeticMap.put("add", "@SP\n" +
--- src/Parser.java
+++ src/Parser.java
@@ -12,29 +12,8 @@ public class Parser {
     File output;
     CodeTranslator codeTranslator;
     String fileName;
-    Set arithmeticSet;
-    final String arithmetics = "add,sub,neg,eq,gt,lt,and,or,not";
-    public enum CommandTypes{
-        C_ARITHMETIC("arithmetic"),
-        C_POP("pop"),
-        C_PUSH("push"),
-        C_LABLE("lable"),
-        C_GOTO("goto"),
-        C_IF("if"),
-        C_FUNCTION("function"),
-        C_RETURN("return"),
-        C_CALL("call");
-        private String type;
-
-        CommandTypes(String type) {
-            this.type = type;
-
-        }
-
-    }
 
     public Parser(File file , String fileName) {
-        this.arithmeticSet = new HashSet(Arrays.asList(arithmetics.split(",")));
         this.input = file;
         this.fileName = fileName;
         this.codeTranslator = new CodeTranslator(fileName);
@@ -60,9 +39,10 @@ public class Parser {
                 if(line.length() == 0)
                     continue;
 
-                CommandTypes type = commandType(line);
-                String command = parseCommand(line,type);
+                CodeTranslator.CommandTypes type = codeTranslator.commandType(line);
+                String command = codeTranslator.parseCommand(line,type);
                 writer.println(command);
+                writer.flush();
             }
             writer.println(codeTranslator.getEndLoop());
         } catch (Exception e){
@@ -77,44 +57,6 @@ public class Parser {
                     br.close();
                 } catch (IOException ignore) {}
             }
-        }
-    }
-
-    CommandTypes commandType(String command){
-
-        String type = command.split(" ")[0];
-        if(arithmeticSet.contains(type)){
-            return CommandTypes.C_ARITHMETIC;
-        }
-        try {
-            return CommandTypes.valueOf(type);
-        } catch (Exception ignored){
-            throw new IllegalArgumentException("Illegal command type");
-        }
-
-
-    }
-
-    String parseCommand(String command, CommandTypes type){
-        String[] commandArray = command.split(" ");
-        int index;
-        switch (type){
-            case C_ARITHMETIC:
-                return codeTranslator.translateArithmetic(commandArray[0]);
-            case C_PUSH:
-                index = Integer.parseInt(commandArray[2]);
-                return codeTranslator.translatePush(commandArray[1], index);
-            case C_POP:
-                index = Integer.parseInt(commandArray[2]);
-                return codeTranslator.translatePop(commandArray[1],index);
-            case C_LABLE:
-            case C_GOTO:
-            case C_IF:
-            case C_FUNCTION:
-            case C_RETURN:
-            case C_CALL:
-            default:
-                throw new IllegalArgumentException("un supported command type");
         }
     }
 }
